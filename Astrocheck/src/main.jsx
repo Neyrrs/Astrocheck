13c2c4e45ec32788b3650c5506e40ccf34348b72
Neyrrrs <neyrrrs@gmail.com>
2024-12-01 20:15:19 +0700

    History patch 2

--- Astrocheck/src/main.jsx
+++ Astrocheck/src/main.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router";
 
 import "./index.css";
 import Home from "./Pages/Home";
-import Login from "./Pages/Login";
+import History from "./Pages/History";
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/History" element={<History />} />
     </Routes>
   </BrowserRouter>
 );
