a0e58f93207152a05ff7f0c6ab8f83fb18201adc
Neyrrrs <neyrrrs@gmail.com>
2025-03-03 22:46:06 +0700

    Update alias

--- Astrocheck/src/Components/Fragments/CardsPack/NavigationBar/CardNavbar.jsx
+++ Astrocheck/src/Components/Fragments/CardsPack/NavigationBar/CardNavbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { ProfileImage } from "@Components/Elements/Icons";
+import { ProfileImage } from "@/Components/Elements/Icons";
 import {
   KartuPerpustakaan,
   Keluar,
