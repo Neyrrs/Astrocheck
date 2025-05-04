d4391f24478b3aee3df2d6452306afad66365549
birugh <notblue.https@gmail.com>
2025-05-04 11:45:09 +0700

    Add New Middleware for parsing body form

--- server/app.js
+++ server/app.js
@@ -15,6 +15,7 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/user", AccountRoute);
 app.use("/major", MajorRoute);
 app.use("/presence", PresenceRoute);
